d597bb713a2d6b68c3b644c130f700fb95744e82
Karastke <fkdlsaos1@naver.com>
2024-06-01 21:49:07 +0900

    refactor: 코드 간결화 및 디자인 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,32 +1,27 @@
 import HeaderNav from '@/app/components/HeaderNav'
 import FooterComponent from '@/app/components/FooterComponent'
-import Categories from '@/app/components/Categories/Categories'
 import '@/app/styles/globals.css'
 import HeaderContent from './components/HeaderContent'
 import Search from './components/Search'
-import Grid from '@/app/components/slider/Grid'
-import Category from '@/app/components/Categories/Category'
+
 
 const RootLayout = ({ children }) => {
   return (
     <html lang='en'>
       <body>
         <div className='flex flex-col bg-white min-h-screen relative'>
-          <header className='fixed bg-white w-full main-padding-list z-40'>
+          <header className='fixed bg-white w-full main-padding-list z-30'>
             <div className='flex items-center justify-between relative py-5'>
               <HeaderContent />
               <Search />
             </div>
           </header>
-          <div className='flex justify-center mt-12 z-10'>
-            <Category />
-          </div>
           {/* main 요소에 relative와 z-index 추가 */}
-          <main className='my-5 w-full flex justify-center main-padding-list relative z-20'>
+          <main className='my-48 justify-center main-padding-list relative'>
             {children}
           </main>
           
-          <FooterComponent />
+          {/* <FooterComponent /> */}
         </div>
       </body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,17 @@
-import Category from './components/Categories/Category'
-import Grid from './components/slider/Grid'
+import Categories from './components/Categories/Categories'
+import CardSlider from './components/slider/CardSlider'
+
 
 export default function Home() {
   // (1) FIXME : Root Page.tsx에 Category와 Grid 들어가야 함 
   // TODO : Grid 컴포넌트명 변경
   return (
-    <div className='w-full'>
-      <div className='flex justify-center mt-12 relative z-10'>
-        <Category />
-      </div>
-      <Grid></Grid>
+    <div>
+      
+        <Categories />
+        <CardSlider />
+      
+      
     </div>
   )
 }

3de1fb4327f0f82cd354495b7101b7c2d520d359
Karastke <fkdlsaos1@naver.com>
2024-06-01 21:36:40 +0900

    feat: 필터 카테고리 추가

---
+++ src/app/components/Categories/Filter/FilterCategories.jsx
@@ -0,0 +1,43 @@
+'use client';
+
+import React, { useState, useEffect } from 'react';
+import FilterCategoriesModal from './FilterCategoriesModal';
+
+function FilterCategories() {
+  const [isFilterModalOpen, setFilterModalOpen] = useState(false);
+
+  const handleOpenFilterModal = () => {
+    setFilterModalOpen(true);
+  };
+
+  const handleCloseFilterModal = () => {
+    setFilterModalOpen(false);
+  };
+
+  useEffect(() => {
+    if (isFilterModalOpen) {
+      document.body.style.overflow = 'hidden';
+    } else {
+      document.body.style.overflow = 'auto';
+    }
+    return () => {
+      document.body.style.overflow = 'auto';
+    };
+  }, [isFilterModalOpen]);
+
+  return (
+    <div className="p-1">
+      <button
+        onClick={handleOpenFilterModal}
+        className="flex items-center px-5 py-3 border text-md font-semibold border-gray-300 bg-white text-black rounded-xl hover:border-black hover:bg-gray-100"
+      >
+        <img src="/assets/categories-icons/filter.svg" alt="필터 이미지" className='w-6 mr-1' />
+        필터
+      </button>
+
+      <FilterCategoriesModal isOpen={isFilterModalOpen} onClose={handleCloseFilterModal} />
+    </div>
+  );
+}
+
+export default FilterCategories;

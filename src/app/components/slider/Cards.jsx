2b6af73a2f58fcd755857000d6b87d895ddf00c9
Karastke <fkdlsaos1@naver.com>
2024-06-03 00:21:45 +0900

    feat: 리스트페이지 cardSlider에 게스트 선호 추가
    Accommodation.json에서 guestFavorite가 true일 경우 게스트 선호가 나타나도록 컴포넌트 제작

--- src/app/components/slider/Cards.jsx
+++ src/app/components/slider/Cards.jsx
@@ -2,16 +2,24 @@
 
 import React from 'react'
 import CardImageBox from './CardImageBox'
+import GuestFavorite from './GuestFavorite'
 
 export default function Cards({ accommodationInfo }) {
   return (
     <div className='card'>
-      <ul className='card-wrap grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 gap-2'>
+      <ul className='card-wrap grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-2'>
         {accommodationInfo.map((item) => (
           <li className='card-item p-2' key={item.accommodationId}>
-            <CardImageBox images={item.imageUrl} />
+            <div className='relative'>
+              <CardImageBox images={item.imageUrl} />
+              {item.guestFavorite && (
+                <div>
+                  <GuestFavorite />
+                </div>
+              )}
+            </div>
             <div className='txt mt-2 text-black'>
-              <h3 className='title  font-semibold'>{item.accommodationName}</h3>
+              <h3 className='title font-semibold'>{item.accommodationName}</h3>
               <div className='host text-gray-600'>
                 <p>호스트: {item.hostName} 님</p>
               </div>

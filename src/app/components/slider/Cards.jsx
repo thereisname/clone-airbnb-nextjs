b465e631d3bf02500d9935500ba207b1f328af74
thereisname <happycm1210@naver.com>
2024-06-02 19:08:18 +0900

    feat: <li> 태그 <Link>태그로 변경
    list 페이지에서 항목 클릭 시 rooms detail page로 이동

--- src/app/components/slider/Cards.jsx
+++ src/app/components/slider/Cards.jsx
@@ -2,13 +2,18 @@
 
 import React from 'react'
 import CardImageBox from './CardImageBox'
+import Link from 'next/link'
 
 export default function Cards({ accommodationInfo }) {
   return (
     <div className='card'>
       <ul className='card-wrap grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 gap-2'>
         {accommodationInfo.map((item) => (
-          <li className='card-item p-2' key={item.accommodationId}>
+          <Link
+            href={`rooms/${item.accommodationId}`}
+            className='card-item p-2'
+            key={item.accommodationId}
+          >
             <CardImageBox images={item.imageUrl} />
             <div className='txt mt-2 text-black'>
               <h3 className='title  font-semibold'>{item.accommodationName}</h3>
@@ -17,7 +22,7 @@ export default function Cards({ accommodationInfo }) {
               </div>
               <span className='price font-bold'>${item.pricePerDay}</span>
             </div>
-          </li>
+          </Link>
         ))}
       </ul>
     </div>

06739f407b70a66293ad6ffab17a51c801d6e83d
Dradradre <leeyonghak13@gmail.com>
2024-06-04 01:32:59 +0900

    Fix: DetailCalendar 수정
    디테일 페이지에서도 가운데로 정렬되게끔 수정

--- src/app/rooms/[accommodationId]/DetailCalendar.jsx
+++ src/app/rooms/[accommodationId]/DetailCalendar.jsx
@@ -1,9 +1,10 @@
+// DetailCalendar.js
 'use client'
 import React, { useState } from 'react'
-import { format, differenceInDays, addMonths, subMonths, isBefore } from 'date-fns'
+import { format, differenceInDays, addMonths, isBefore } from 'date-fns'
 import Calendar from './CalendarLayout'
 
-const DetailCalendar = ({ py }) => {
+const DetailCalendar = ({ py, className }) => {
   const [currentMonth, setCurrentMonth] = useState(new Date())
   const [checkInDate, setCheckInDate] = useState(null)
   const [checkOutDate, setCheckOutDate] = useState(null)
@@ -34,20 +35,20 @@ const DetailCalendar = ({ py }) => {
   const nextMonth = addMonths(currentMonth, 1)
 
   return (
-    <div className={`bg-white rounded-lg ${py}`}>
+    <div className={`bg-white rounded-lg ${py} ${className}`}>
       <div className='text-left mb-4'>
-        <h1 className='text-[22px]'>가평군에서 {calculateNights()}박</h1>
+        <h1 className='text-[22px] text-center'>가평군에서 {calculateNights()}박</h1>
         {checkInDate && checkOutDate ? (
-          <p className='text-gray-600 text-[14px] pt-2'>
+          <p className='text-gray-600 text-[14px] pt-2 text-center'>
             {format(checkInDate, 'yyyy년 MMMM d일')} - {format(checkOutDate, 'yyyy년 MMMM d일')}
           </p>
         ) : (
-          <div className='text-gray-600 text-[14px] pt-2'>
+          <div className='text-gray-600 text-[14px] pt-2 text-center'>
             여행 날짜를 입력하여 정확한 요금을 확인하세요.
           </div>
         )}
       </div>
-      <div className='flex space-x-10'>
+      <div className='flex justify-center space-x-10'>
         <div className='relative pb-10'>
           <Calendar
             currentMonth={currentMonth}

56e5ed6afecd4072f0604286939e2695528ec0b3
Dradradre <leeyonghak13@gmail.com>
2024-06-04 08:56:29 +0900

    Feat: util.js에 calculateNights 함수 추가
    calculateNights 함수는 박 수(night)를 계산하는 함수

--- src/common/utils.js
+++ src/common/utils.js
@@ -1,4 +1,16 @@
-export default function formatPrice(price) {
+import { differenceInDays } from "date-fns"
+
+export const formatPrice = (price) => {
     return new Intl.NumberFormat('ko-KR', { style: 'currency', currency: 'KRW' }).format(price)
   } // 10000을 ₩10,000으로 변환하는 함수
   
+
+  export const calculateNights = (checkInDate, checkOutDate) => {
+    if (!checkInDate || !checkOutDate) {
+      return 1
+    }
+    if (checkInDate && checkOutDate) {
+      return differenceInDays(checkOutDate, checkInDate)
+    }
+    return 0
+  } // 박 수(night) 계산 함수

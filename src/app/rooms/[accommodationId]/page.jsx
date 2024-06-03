1bac60c6203cdf97ed971c19c8676707b4f34196
Dradradre <leeyonghak13@gmail.com>
2024-06-03 20:18:46 +0900

    Fix: DetailLayout에 있던 내용 page.jsx로 병합

--- src/app/rooms/[accommodationId]/page.jsx
+++ src/app/rooms/[accommodationId]/page.jsx
@@ -1,8 +1,16 @@
 'use client'
 
 import React, { useEffect, useState } from 'react'
-import DetailLayout from './DetailLayout'
 import { useParams } from 'next/navigation'
+import DetailTitle from './DetailTitle'
+import DetailGallery from './DetailGallery'
+import DetailFeatures from './DetailFeature'
+import DetailHostInfo from './DetailHostInfo'
+import DetailSubTitle from './DetailSubTitle'
+import DetailReview from './DetailReview'
+import DetailCalendar from './DetailCalendar'
+import DetailAccommodationInfo from './DetailAccommodation'
+import ReservationComponent from './ReservationComponent'
 
 const fetchAccommodation = async (accommodationId) => {
   const res = await fetch(
@@ -15,6 +23,7 @@ const fetchAccommodation = async (accommodationId) => {
 }
 
 const AccommodationDetailPage = () => {
+  // @ts-ignore
   const { accommodationId } = useParams()
   const [accommodation, setAccommodation] = useState(null)
   const [error, setError] = useState(null)
@@ -42,7 +51,29 @@ const AccommodationDetailPage = () => {
     return <div>Loading...</div>
   }
 
-  return <DetailLayout accommodation={accommodation} />
+  return (
+    <div className='main-padding-detail'>
+      <div className='max-w-full xl:w-[1120px] mx-auto'>
+        <DetailTitle accommodation={accommodation} />
+        <DetailGallery images={accommodation.imageUrl} />
+        <div className='flex flex-wrap my-10'>
+          <div className='flex-1'>
+            <DetailSubTitle accommodation={accommodation} />
+            <DetailFeatures amenities={accommodation.amenities} />
+            <DetailHostInfo hostName={accommodation.hostName} hostSince={accommodation.hostSince} />
+          </div>
+          <div className='flex justify-center md:justify-end w-full md:w-auto'>
+            <ReservationComponent pricePerDay={accommodation.pricePerDay} />
+          </div>
+        </div>
+        <DetailCalendar py='py-12' />
+        <hr />
+        <DetailReview reviews={accommodation.reviews} />
+        <hr />
+        <DetailAccommodationInfo desc={accommodation.desc} />
+      </div>
+    </div>
+  )
 }
 
 export default AccommodationDetailPage

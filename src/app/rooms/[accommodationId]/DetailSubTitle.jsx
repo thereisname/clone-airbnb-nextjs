0c25e449e2cc266c0d601267e61cb275a4ac8855
quswjdgns399 <quswjdgns3996@gmail.com>
2024-06-03 20:33:43 +0900

    Feat: DetailSubTitle후기 개수 추가, DetailReviewModal 연결

--- src/app/rooms/[accommodationId]/DetailSubTitle.jsx
+++ src/app/rooms/[accommodationId]/DetailSubTitle.jsx
@@ -1,10 +1,41 @@
-import React from 'react'
+// import React from 'react'
+
+// const DetailSubTitle = ({ accommodation }) => {
+//   return (
+//     <div>
+//       <h2 className='text-lg font-semibold mb-4'>{accommodation.locationName}</h2>
+//       <p>{accommodation.briefRoomInfo.join(', ')}</p>
+//       <button
+//         onClick={() => {
+//           return <DetailSubTitle accommodation={accommodation} reviewCount={reviewCount} />
+//         }}
+//         className='text-[16px] underline bold mt-4'
+//       >
+//         리뷰 {accommodation.reviewCount}
+//       </button>
+//     </div>
+//   )
+// }
+
+import React, { useState } from 'react'
+import DetailReviewModal from '@/app/rooms/[accommodationId]/DetailReviewModal'
 
 const DetailSubTitle = ({ accommodation }) => {
+  const [isModalOpen, setIsModalOpen] = useState(false)
+
+  const toggleModal = () => {
+    setIsModalOpen(!isModalOpen)
+  }
+
   return (
     <div>
-      <h2 className='text-lg font-semibold mb-4'>{accommodation.locationName}</h2>
-      <p>{accommodation.briefRoomInfo.join(', ')}</p>
+      <h2 className='text-lg font-semibold'>{accommodation.locationName}</h2>
+      <div>{accommodation.briefRoomInfo.join(', ')}</div>
+      <button onClick={toggleModal} className='text-[16px] underline text-bold'>
+        후기 {accommodation.reviewCount}개
+      </button>
+
+      {isModalOpen && <DetailReviewModal onClose={toggleModal} />}
     </div>
   )
 }

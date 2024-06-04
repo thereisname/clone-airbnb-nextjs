2174a9277f535152b9c00e66a1e98c2ddeb440e7
thereisname <happycm1210@naver.com>
2024-06-04 16:39:07 +0900

    config: next config and tailwind config
    next config에 domains를 추가하여 cors 해결 및 tailwind config에 background color 원하는 색상코드와 name을 설정함

--- next.config.mjs
+++ next.config.mjs
@@ -1,20 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    async redirects() {
-      return [
-        {
-          source: '/components/:path*',
-          destination: '/',
-          permanent: true,
-        },
-        {
-          source: '/styles/:path*',
-          destination: '/',
-          permanent: true,
-        },
-      ]
-    },
-  }
-  
-  export default nextConfig
-  
+  async redirects() {
+    return [
+      {
+        source: '/components/:path*',
+        destination: '/',
+        permanent: true,
+      },
+      {
+        source: '/styles/:path*',
+        destination: '/',
+        permanent: true,
+      },
+    ]
+  },
+  images: {
+    domains: ['a0.muscache.com'],
+  },
+}
+
+export default nextConfig

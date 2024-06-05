c241e2f5420d33b8117034eaad78ba73de760c5b
karastke <fkdlsaos1@naver.com>
2024-06-05 13:18:52 +0900

    feat: 카드 슬라이더 이미지에 하트 컴포넌트 추가

--- src/app/components/slider/Cards.jsx
+++ src/app/components/slider/Cards.jsx
@@ -3,6 +3,7 @@
 import React from 'react'
 import CardImageBox from './CardImageBox'
 import GuestFavorite from './GuestFavorite'
+import HeartFavorite from './HeartFavorite'
 
 export default function Cards({ accommodationInfo }) {
   return (
@@ -12,11 +13,14 @@ export default function Cards({ accommodationInfo }) {
           <li className='card-item p-2' key={item.accommodationId}>
             <div className='relative'>
               <CardImageBox images={item.imageUrl} />
-              {item.guestFavorite && (
-                <div>
-                  <GuestFavorite />
-                </div>
-              )}
+              <div className='z-20 grid grid-cols-2'>
+                {item.guestFavorite && (
+                  <div className='absolute top-1 w-full flex justify-between '>
+                    <GuestFavorite />
+                  </div>
+                )}
+                <HeartFavorite />
+              </div>
             </div>
             <div className='txt mt-2 text-black'>
               <h3 className='title font-semibold'>{item.accommodationName}</h3>

7d3be5d407f023ea020d36a64f4f8db95796390d
thereisname <happycm1210@naver.com>
2024-06-08 13:35:24 +0900

    feat: list page와 detail page header의 padding을 각각 다르게 설정
    usePathname으로 페이지 정보를 가져와 main-padding-list와 main-padding-detail을 페이지에 맞게 설정함

--- src/app/components/DynamicHeader.jsx
+++ src/app/components/DynamicHeader.jsx
@@ -4,8 +4,11 @@ import { useEffect, useState } from 'react'
 import Header from './HeaderContent'
 import SearchBar from './Search'
 import CompactSearchBar from '@/app/components/CompactSearchBar'
+import { usePathname } from 'next/navigation'
 
 const DynamicHeader = () => {
+  const pathname = usePathname()
+  const isAccommodationDetailPage = pathname && pathname.startsWith('/rooms/')
   const [scroll, setScroll] = useState(0)
 
   useEffect(() => {
@@ -22,9 +25,13 @@ const DynamicHeader = () => {
 
   return (
     <header className={`${scroll > 2 ? 'h-[78px] pb-[78px]' : 'h-52 lg:h-40 pb-52 lg:pb-40'}`}>
-      <div className={`fixed top-0 left-0 bg-white w-full z-20 main-padding-list`}>
-        <div className='flex items-center justify-between relative py-4'>
-          <Header />
+      <div
+        className={`fixed top-0 left-0 bg-white w-full z-20 ${isAccommodationDetailPage ? 'main-padding-detail' : 'main-padding-list'}`}
+      >
+        <div className={`flex items-center justify-center relative py-4`}>
+          <div className={`${isAccommodationDetailPage ? 'w-full xl:w-[1120px]' : 'w-full'}`}>
+            <Header />
+          </div>
           {scroll > 2 ? <CompactSearchBar /> : <SearchBar />}
         </div>
       </div>

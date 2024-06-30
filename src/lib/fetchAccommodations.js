8fa44d28a8ac4c217a1d49f998e092894fbec85a
thereisname <happycm1210@naver.com>
2024-07-01 01:46:45 +0900

    add : add fetch Method - lib/fetchAccommodation

--- src/lib/fetchAccommodations.js
+++ src/lib/fetchAccommodations.js
@@ -7,3 +7,13 @@ export const fetchAccommodations = async (accommodationId) => {
   const data = await response.json()
   return data
 }
+
+export const fetchAccommodation = async () => {
+  const response = await fetch(`http://localhost:8080/api/accommodations`)
+
+  if (!response.ok) {
+    throw new Error('Failed to fetch accommodations')
+  }
+  const data = await response.json()
+  return data
+}
